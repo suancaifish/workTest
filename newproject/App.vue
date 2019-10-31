7fe068b71b501d72d4f877bb2ff045826b4e2259
ouzf <ouzf@weixue100.com>
2019-10-31 18:24:31 +0800

    创建cjw分支

--- newproject/App.vue
+++ newproject/App.vue
@@ -14,9 +14,9 @@ export default {
   },
   onHide: function() {
     console.log("App Hide");
-    log.info("user info2"); // 日志会和当前打开的页面关联，建议在页面的onHide、onShow等生命周期里面打
-    log.error("error2");
-    log.warn("sss2");
+    log.info("user info"); // 日志会和当前打开的页面关联，建议在页面的onHide、onShow等生命周期里面打
+    log.error("error");
+    log.warn("sss");
   }
 };
 </script>
